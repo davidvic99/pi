75b146291a3c1eaa273f6dce6d9e68db77a5042e
vicente98fg <43605917+vicente98fg@users.noreply.github.com>
2019-03-25 13:33:58 +0100

    Add files via upload
    ORDENADO LOKO

---
+++ Proyecto Integrado GitHub/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Page Title</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="./css/estiloIndex.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="main.js"></script>
+</head>
+<body>
+
+<div class="logo">
+
+<img src="./img/florida.png" width="300px" height="150px"></img>
+
+</div>
+
+<div class="botones">
+
+<form action="">
+  <input type="button" name="login" value="Login"> 
+    <br><br>
+  <input type="button" name="register" value="Registrarse">
+</form>
+
+</div>
+
+<div id="footer">
+
+No se que no se cuantos Florida
+
+</div>
+
+</body>
+</html>
+
---
+++ Proyecto Integrado GitHub/main.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Main</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/estiloMain.css"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<div id="userInfo">
+    <div id="puntosUsuario">Puntos: 45🔥</div>
+    <div id="nivel">Nivel: Bronce<img id="bronceM" src="./img/bronce.png"></div>
+    <a href="perfil.php"><img src="img/user.jpg" id="userimg"></a>
+</div>  
+<div id="form">
+<form id="intCodigo" action="hola.php" method="POST" name="codigo" >
+    <p class="tituloForm"><b>Introducir Código</b></p>
+    <input type="text" name="codigo" placeholder="0123-4567-8901-2345" pattern="[0-9&-]{19}" required>
+    <input type="submit" value="Validar">
+    <div id="info">
+    <p class="rojo">El codigo fue usado/ no es valido. <span class="verde">El codigo es correcto</span></p>
+</div>
+</form>
+
+</div>
+<div id="ofertas">
+<div id="ofertabronce">
+    <h2>Ofertas Bronce 🔓</h2>
+    <hr width="95%" />
+    <br>
+    <img id="oferta1" src="./img/ejemplo.png"><p id="ofertas1txt">
+        <ul id="ofertas1txt">
+            <li>Nombre: Ejemplo</li>
+            <li>Descripcion: ejemplo</li>
+            <li>Precio: 15 💸</li>
+            <a href="oferta1.php">Usar</a>
+
+        </ul></p>
+</div>
+<div id="ofertasplata">
+<h2>Ofertas Plata 🔒 </h2>
+<hr width="95%" />
+<br>
+<img id="oferta1" src="./img/candado.png"><p id="ofertas1txt">
+<ul id="ofertas1txt">
+            <li>Nombre: Ejemplo</li>
+            <li>Descripcion: ejemplo</li>
+            <li>Precio: 7 💸</li>
+            <a href="oferta1.php">Bloqueado</a>
+
+        </ul></p>
+</div>
+
+<div id="ofertasoro">
+<h2>Ofertas Oro 🔒</h2>
+<hr width="95%" />
+<br>
+<img id="oferta1" src="./img/candado.png"><p id="ofertas1txt">
+<ul id="ofertas1txt">
+            <li>Nombre: Ejemplo</li>
+            <li>Descripcion: ejemplo</li>
+            <li>Precio: 4 💸</li>
+            <a href="oferta1.php">Bloqueado</a>
+
+        </ul></p>
+</div>
+
+</div>
+</body>
+</html>
