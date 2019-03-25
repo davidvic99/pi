1fca398abbe7e18bbdd6a8a5ecd90c45e2793226
robervip17 <44229332+robervip17@users.noreply.github.com>
2019-03-25 13:37:07 +0100

    Robertillo

---
+++ Proyectointegrado/public/perfil.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="public/css/estiloPerfil.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title></title>
+  </head>
+  <body>
+    <img id="perfil" src="public/img/user.jpg" alt="">
+    <div class="info">Nombre y Apellido <br>
+    <i>DNI</i>
+    </div>
+    <input type="submit" name="editarPerfil" value="Editar Perfil">
+    <img id="nivel" src="public/img/bronce.jpg" alt="">
+    <div class="nivel"><b>Nivel</b><br>
+    Bronce</div>
+    <div class="puntosAc"><b>Puntos Actuales</b><br>
+    45</div>
+    <div class="puntosNe"><b>Puntos necesarios siguiente nivel</b><br>
+    55</div>
+    <div class="ProcesoNivel"><b>Proceso de nivel:</b></div>
+    <div id="barraPro"><div class="w3-container">
+    <div class="w3-light-grey">
+    <div class="w3-container w3-blue" style="width:45%">45%</div>
+    </div><br>
+  </div></div>
+  <div class="adInfo"><b>Información Adicional</b><br></div>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><hr>
+  </body>
+</html>
