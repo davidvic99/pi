b12b6440118db45423a646602a5251ef4c048856
davidvic99 <43604557+davidvic99@users.noreply.github.com>
2019-03-28 13:48:56 +0100

    Este

---
+++ Proyecto Integrado GitHub DEFINITIVO/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Page Title</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="./css/estiloIndex.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="main.js"></script>
+</head>
+<body>
+
+<div class="logo">
+
+<img src="./img/florida.png" width="300px" height="150px"></img>
+
+</div>
+
+<div class="botones">
+
+<form action="">
+  <a href="login.php"><input type="button" name="login" value="Login"> </a>
+    <br><br>
+ <a href="registro.php"> <input type="button" name="register" value="Registrarse"></a>
+</form>
+
+</div>
+
+<div id="footer">
+
+<p>Florida Universitaria © 2019 - AppDescuentos </p>
+
+</div>
+
+</body>
+</html>
+
---
+++ Proyecto Integrado GitHub DEFINITIVO/registro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./css/estiloRegistro.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title></title>
+  </head>
+  <body>
+    <img src="./img/florida.png" alt="">
+    <form class="" action="" method="post">
+    <div class="texto">
+        Usuario:
+        <input type="text" name="usuario" value="" required><br><br><br>
+        Nombre:
+        <input type="text" name="nombre" value="" required><br><br><br>
+        Apellidos:
+        <input type="text" name="apellido" value="" required><br><br><br>
+        DNI:
+        <input type="text" name="usuario" value="" required><br><br><br>
+        E-mail:
+        <input type="email" name="email" value="" required><br><br><br>
+        Estudios:
+        <select name="transporte" required >
+          <option selected>DAW</option>
+          <option>3D</option>
+          <option>ASIR</option>
+          <option>DAM</option>
+        </select><br><br>
+        Contraseña:
+        <input type="password" name="contraseña" value="" required><br><br><br>
+        Repetir contraseña:
+        <input type="password" name="contraseña2" value="" required><br><br><br>
+        <input type="submit" name="button" value="Enviar"><br><br><br>
+    </form></div>
+</form>
+  </body>
+</html>
