7e829df137bc78eb84eb5a3df77cf4ce877451e2
ALUMNADO\armoro <arnaumorant@hotmail.com>
2019-05-14 13:41:26 +0200

    Soporte a iPhone 5 añadido

--- public/main.php
+++ public/main.php
@@ -6,7 +6,7 @@
     <title>Main</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="./css/estiloMain.css">
-
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
 <body>
 
