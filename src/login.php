647e17bf685686affa85b7e6f188e1609e922511
ALUMNADO\armoro <arnaumorant@hotmail.com>
2019-05-15 12:15:42 +0200

    Añado soporte para responsive en todas las paginas y añado responsive para login.php

--- src/login.php
+++ src/login.php
@@ -7,7 +7,7 @@ class Login extends conexion{
     public function iniciarSesion($conn,$post){
 
         if (isset($post['usuario'])) {
-                
+
             $usuario = $_REQUEST['usuario'];
             $password = sha1($_REQUEST['password']);
             $result = mysqli_query($conn,"SELECT * FROM `users` WHERE usuario='$usuario'and password='$password'") or die(mysql_error());
@@ -15,18 +15,16 @@ class Login extends conexion{
             if($rows==1){
 
                 session_start();
-            
+
                 $_SESSION['usuario'] = $usuario;
-                header( "refresh:0.001;url=main.php" ); 
-             
+                header( "refresh:0.001;url=main.php" );
+
                 return $_SESSION['usuario'];
-             
-             
-              
+
             }else{
-                echo "Error en el nombre de usuario/contraseña";
+              echo "<div class='error' style='display:inherit;'>";
             }
-         
+
         }else{
             "Tienes que rellenar todos los campos";
         }
