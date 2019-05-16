8ebed2b4389f04b1eb6b957316d1997e78847b93
vicente98fg <43605917+vicente98fg@users.noreply.github.com>
2019-05-16 13:45:30 +0200

    Actualizacion perfil y editar perfil

--- public/perfil.php
+++ public/perfil.php
@@ -1,3 +1,19 @@
+<?php
+
+
+require_once('../src/infoUsuario.php');
+
+$conexion = new Conexion();
+$bbdd = $conexion->conectar();
+$session = $conexion->comprobarSession($_SESSION["usuario"]);
+
+$infou = new InfoUsuario();
+$setInfo = $infou -> usuarioInfo($bbdd, $_SESSION["usuario"]);  
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
   <head>
@@ -8,12 +24,12 @@
   </head>
   <body>
     <img id="perfil" src="./img/user.jpg" alt="">
-    <div class="info"><b>Nombre y Apellido</b>: Robertito el maki <br>
-    <i><b>DNI:</b> 2102301302W</i>
+    <div class="info">Nombre y Apellido:   EJEMPLO EJEMPLO <br>
+    <i>DNI: 2102301302W</i>
     </div>
     <br>
 
-    <a href="editarPerfil.php"><input class="boton1" type="submit" name="editarPerfil" value="Editar Perfil"></a>
+    <a href="editarPerfil.php   "><input type="submit" name="editarPerfil" value="Editar Perfil"></a>
     <img id="nivel" src="./img/bronce.png" alt="">
     <div class="nivel"><b>Nivel</b><br>
     Bronce</div>
@@ -21,21 +37,27 @@
     45</div>
     <div class="puntosNe"><b>Puntos necesarios siguiente nivel</b><br>
     55</div>
-    <div class="ProcesoNivel"><b>Proceso de nivel:</b><br>45%</div>
-    <progress max="100" value="45"></progress><br>
-  <a href="main.php"><input class="boton2" type="submit" name="volver" value="Volver"></a>
+    <div class="ProcesoNivel"><b>Proceso de nivel:</b></div>
+    <div id="barraPro"><div class="w3-container">
+    <div class="w3-light-grey">
+    <div class="w3-container w3-blue" style="width:45%">45%</div>
+    </div><br>
+  </div></div>
+  <a href="main.php   "><input type="submit" name="volver" value="Volver"></a>
   <br>
   <div class="adInfo"><b>Información Adicional</b><br></div>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><hr>
   <div id="centro">
   <p>
-    <p><b>Nombre</b>: Ejemplo</p>
-    <p><b>Apellidos</b>: Ejemplo</p>
-    <p><b>DNI</b>: Ejemplo</p>
-    <p><b>Estudios</b>: DAW</p>
-    <p><b>Correo</b>: yomismo@yomismo.cat</p>
-    <p><b>Ciudad</b>: Valencia</p>
+    <p><b>Información:</b></p>
+    <p>Nombre: <?php echo $setInfo[0]?></p>
+    <p>Apellidos: <?php echo $setInfo[1]?></p>
+    <p>DNI: <?php echo $setInfo[8]?></p>
+    <p>Estudios: <?php echo $setInfo[3]?></p>
+    <p>Correo: <?php echo $setInfo[5]?></p>
 
     </p>
     </div>
+
   </body>
 </html>
