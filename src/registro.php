ba9d926ef3872b536337fd5fa744c4e900fdf58b
DAVID VICTORIA MARTINEZ <davima@floridauniversitaria.es>
2019-05-17 11:07:23 +0200

    registro cambio en los mensajes

--- src/registro.php
+++ src/registro.php
@@ -10,7 +10,7 @@ class Registro extends Conexion{
 
     function registrar($conn, $post){
 
-        
+        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@8'></script>";
         if ((isset($post['nombre']))&&(isset($post['usuario']))&&(isset($post['apellido']))&&(isset($post['dni']))&&(isset($post['estudios']))&&(isset($post['contraseña']))&&(isset($post['contraseña2']))) {
 
             $contraseña = $post['contraseña'];
@@ -25,22 +25,51 @@ class Registro extends Conexion{
                     $contraseña = sha1($contraseña);
                  
                     if($insertar =  mysqli_query($conn, "INSERT INTO `users` (`id`, `nombre`, `apellido`, `usuario`, `email`, `estudios`, `password`, `nivel`, `puntos`, `Registro`, `rango`, `nivelExp`, `dni`) VALUES (NULL, '$nombre', '$apellido', '$usuario', '$email', '$estudios', '$contraseña', '1', '0', CURRENT_TIMESTAMP, 'Bronce', '1', '$dni')")){
-                        echo "<h2>Tu cuenta se ha creado correctamente.</h2>";
+                        
+                    echo "<script>
+                    Swal.fire({
+                type: 'success',
+                title: 'Genial!',
+                text: 'Cuenta creada correctamente.',
+                })
+                </script>";
+                        
                     }else{
-                        echo "<h2>Una cuenta con este nombre de usuario/dni ya existe.</h2>";
+                        echo "<script>Swal.fire({
+                            type: 'error',
+                            title: 'Oops...',
+                            text: 'Ya exite una cuenta con ese nombre de usuario / DNI '
+                          
+                          })
+                              </script>";
+                      
                     }
        
                   
                    
                 }else{
 
-                    echo "Las contraseñas no coinciden.";
+                    echo "<script>Swal.fire({
+                        type: 'error',
+                        title: 'Oops...',
+                        text: 'Las contraseñas introducidas ya existen.'
+                      
+                      })
+                          </script>";
+                  
                 }
                
 
             }else{
 
-                echo "Fallo en el proceso de Registro. Verifica todos los campos";
+                echo "<script>Swal.fire({
+                    type: 'error',
+                    title: 'Oops...',
+                    text: 'Fallo en el registro, verifica todos los campos  '
+                  
+                  })
+                      </script>";
+              
             }
 
     }
