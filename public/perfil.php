f4fd254a3732f531b7a960b57689e38f3caaf9fc
BuildTools <unconfigured@null.spigotmc.org>
2019-05-22 00:43:28 +0200

    Creado responsive y estilos perfil

--- public/perfil.php
+++ public/perfil.php
@@ -2,41 +2,45 @@
 <!-- saved from url=(0058)http://localhost/Programaci%c3%b3nPHP/PI/public/perfil.php -->
 <html lang="es" dir="ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-    <link rel="stylesheet" href="./css/estiloPerfil1.css">
+    <link rel="stylesheet" href="./css/estiloPerfil.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <title>Perfil</title>
   </head>
   <body>
-    <img id="perfil" src="./img/user.jpg" alt="">
-    <div class="info"><b>Nombre y Apellido</b>: Robertito el maki <br>
-    <i><b>DNI:</b> 2102301302W</i>
+    <div class="cajaGrande">
+      <div class="cajaArribaIzq">
+        <img id="perfil" src="./img/user.jpg" alt="">
+        <div class="info"><b>Nombre y Apellido</b>: Robertito <br>
+        <i><b>DNI:</b> 2102301302W</i></div></div>
+      <div class="cajaArribaDer">
+        <a href="http://localhost/Programaci%c3%b3nPHP/PI/public/editarPerfil.php"><input class="boton1" type="submit" name="editarPerfil" value="Editar Perfil"></a>
+        <a href="http://localhost/Programaci%c3%b3nPHP/PI/public/main.php"><input class="boton2" type="submit" name="volver" value="Volver"></a>
+      </div>
+      <div class="cajaMedioIzq">
+        <img id="nivel" src="./img/bronce.png" alt="">
+        <div class="nivel"><b>Nivel</b><br>
+        Bronce</div></div>
+      <div class="cajaMedio">
+        <div class="puntosAc"><b>Puntos Actuales</b><br>
+        45</div></div>
+      <div class="cajaMedioDer">
+      <div class="puntosNe"><b>Puntos necesarios siguiente nivel</b><br>
+      55</div></div>
+      <div class="cajaPorcentaje">
+      <div class="ProcesoNivel"><b>Proceso de nivel: </b>45%</div>
+      <progress max="100" value="45"></progress><br></div>
+      <div class="cajaAbajo">
+        <b>Información Adicional</b><br>
+        <p>
+          </p><p><b>Nombre</b>: Ejemplo</p>
+          <p><b>Apellidos</b>: Ejemplo</p>
+          <p><b>DNI</b>: Ejemplo</p>
+          <p><b>Estudios</b>: DAW</p>
+          <p><b>Correo</b>: yomismo@yomismo.cat</p>
+          <p><b>Ciudad</b>: Valencia</p>
+          <p></p>
+          </div>
     </div>
-    <br>
-
-    <a href="http://localhost/Programaci%c3%b3nPHP/PI/public/editarPerfil.php"><input class="boton1" type="submit" name="editarPerfil" value="Editar Perfil"></a>
-    <img id="nivel" src="./img/bronce.png" alt="">
-    <div class="nivel"><b>Nivel</b><br>
-    Bronce</div>
-    <div class="puntosAc"><b>Puntos Actuales</b><br>
-    45</div>
-    <div class="puntosNe"><b>Puntos necesarios siguiente nivel</b><br>
-    55</div>
-    <div class="ProcesoNivel"><b>Proceso de nivel:</b><br>45%</div>
-    <progress max="100" value="45"></progress><br>
-  <a href="http://localhost/Programaci%c3%b3nPHP/PI/public/main.php"><input class="boton2" type="submit" name="volver" value="Volver"></a>
-  <br>
-  <div class="adInfo"><b>Información Adicional</b><br></div>
-  <div id="centro">
-  <p>
-    </p><p><b>Nombre</b>: Ejemplo</p>
-    <p><b>Apellidos</b>: Ejemplo</p>
-    <p><b>DNI</b>: Ejemplo</p>
-    <p><b>Estudios</b>: DAW</p>
-    <p><b>Correo</b>: yomismo@yomismo.cat</p>
-    <p><b>Ciudad</b>: Valencia</p>
-
-    <p></p>
-    </div>
-
-
-</body></html>
+</body>
+</html>
