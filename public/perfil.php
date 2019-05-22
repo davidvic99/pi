f0383c5aa904269c04e15ea10439bdf7c6e96c82
ALUMNADO\armoro <arnaumorant@hotmail.com>
2019-05-22 12:05:25 +0200

    Subo la mitad del php de perfil.php

--- public/perfil.php
+++ public/perfil.php
@@ -1,5 +1,16 @@
+<?php
+
+require_once('../src/infoUsuario.php');
+
+
+$conexion = new Conexion();
+$bbdd = $conexion->conectar();
+$session = $conexion->comprobarSession($_SESSION["usuario"]);
+$infou = new InfoUsuario();
+$setInfo = $infou -> usuarioInfo($bbdd, $_SESSION["usuario"]);
+
+?>
 <!DOCTYPE html>
-<!-- saved from url=(0058)http://localhost/Programaci%c3%b3nPHP/PI/public/perfil.php -->
 <html lang="es" dir="ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
     <link rel="stylesheet" href="./css/estiloPerfil.css">
@@ -11,34 +22,33 @@
     <div class="cajaGrande">
       <div class="cajaArribaIzq">
         <img id="perfil" src="./img/user.jpg" alt="">
-        <div class="info"><b>Nombre y Apellido</b>: Robertito <br>
-        <i><b>DNI:</b> 2102301302W</i></div></div>
+        <div class="info"><b>Nombre y Apellido: </b><?php echo $setInfo[0]?> <?php echo $setInfo[1]?><br>
+        <i><b>DNI: </b><?php echo $setInfo[8]?></i></div></div>
       <div class="cajaArribaDer">
-        <a href="./PI/public/editarPerfil.php"><input class="boton1" type="submit" name="editarPerfil" value="Editar Perfil"></a>
-        <a href="./PI/public/main.php"><input class="boton2" type="submit" name="volver" value="Volver"></a>
+        <a href="editarPerfil.php"><input class="boton1" type="submit" name="editarPerfil" value="Editar Perfil"></a>
+        <a href="main.php"><input class="boton2" type="submit" name="volver" value="Volver"></a>
       </div>
       <div class="cajaMedioIzq">
-        <img id="nivel" src="./img/bronce.png" alt="">
+        <img id="nivel" src="./<?php echo $setInfo[11]?>" alt="">
         <div class="nivel"><b>Nivel</b><br>
-        Bronce</div></div>
+        <?php echo $setInfo[7]?></div></div>
       <div class="cajaMedio">
         <div class="puntosAc"><b>Puntos Actuales</b><br>
-        45</div></div>
+        <?php echo $setInfo[5]?></div></div>
       <div class="cajaMedioDer">
       <div class="puntosNe"><b>Puntos necesarios siguiente nivel</b><br>
-      55</div></div>
+       <?php // echo $setInfo[10] ?></div></div>
       <div class="cajaPorcentaje">
-      <div class="ProcesoNivel"><b>Proceso de nivel: </b>45%</div>
-      <progress max="100" value="45"></progress><br></div>
+      <div class="ProcesoNivel"><b>Proceso de nivel: </b><?php // echo $setInfo[4]?>%</div>
+      <progress max="100" value="<?php // echo $setInfo[10]?>"></progress><br></div>
       <div class="cajaAbajo">
         <b>Información Adicional</b><br>
-        <p>
-          </p><p><b>Nombre</b>: Ejemplo</p>
-          <p><b>Apellidos</b>: Ejemplo</p>
-          <p><b>DNI</b>: Ejemplo</p>
-          <p><b>Estudios</b>: DAW</p>
-          <p><b>Correo</b>: yomismo@yomismo.cat</p>
-          <p><b>Ciudad</b>: Valencia</p>
+        <p></p>
+          <p><b>Nombre: </b><?php echo $setInfo[0]?></p>
+          <p><b>Apellidos: </b><?php echo $setInfo[1]?></p>
+          <p><b>DNI: </b><?php echo $setInfo[8]?></p>
+          <p><b>Estudios: </b><?php echo $setInfo[2]?></p>
+          <p><b>Correo: </b><?php echo $setInfo[3]?></p>
           <p></p>
           </div>
     </div>
