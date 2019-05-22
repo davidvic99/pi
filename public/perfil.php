b7280e49fa6277c3c552c8efc78958e5d4c0c914
ALUMNADO\rovipo <vinesroberto13@gmail.com>
2019-05-22 09:41:27 +0200

    Hecho bien el responsive PERFIL (DEFINITIVO)

--- public/perfil.php
+++ public/perfil.php
@@ -1,54 +1,46 @@
-<?php
-require_once('../src/infoUsuario.php');
-
-
-$conexion = new Conexion();
-$bbdd = $conexion->conectar();
-$session = $conexion->comprobarSession($_SESSION["usuario"]);
-
-$infou = new InfoUsuario();
-$setInfo = $infou -> usuarioInfo($bbdd, $_SESSION["usuario"]);
-
-?>
-
-
 <!DOCTYPE html>
+<!-- saved from url=(0058)http://localhost/Programaci%c3%b3nPHP/PI/public/perfil.php -->
 <html lang="es" dir="ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <head>
+
+    <link rel="stylesheet" href="./css/estiloPerfil.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <title>Perfil</title>
-        <link rel="stylesheet" href="./css/estiloPerfil.css">
   </head>
   <body>
-    <img id="perfil" src="./img/user.jpg" alt="">
-    <div class="info"><b>Usuario: </b> <?php echo$_SESSION["usuario"];?>    <br>
-    <i><b>DNI:</b><?php echo $setInfo[8]?></i>
+    <div class="cajaGrande">
+      <div class="cajaArribaIzq">
+        <img id="perfil" src="./img/user.jpg" alt="">
+        <div class="info"><b>Nombre y Apellido</b>: Robertito <br>
+        <i><b>DNI:</b> 2102301302W</i></div></div>
+      <div class="cajaArribaDer">
+        <a href="http://localhost/Programaci%c3%b3nPHP/PI/public/editarPerfil.php"><input class="boton1" type="submit" name="editarPerfil" value="Editar Perfil"></a>
+        <a href="http://localhost/Programaci%c3%b3nPHP/PI/public/main.php"><input class="boton2" type="submit" name="volver" value="Volver"></a>
+      </div>
+      <div class="cajaMedioIzq">
+        <img id="nivel" src="./img/bronce.png" alt="">
+        <div class="nivel"><b>Nivel</b><br>
+        Bronce</div></div>
+      <div class="cajaMedio">
+        <div class="puntosAc"><b>Puntos Actuales</b><br>
+        45</div></div>
+      <div class="cajaMedioDer">
+      <div class="puntosNe"><b>Puntos necesarios siguiente nivel</b><br>
+      55</div></div>
+      <div class="cajaPorcentaje">
+      <div class="ProcesoNivel"><b>Proceso de nivel: </b>45%</div>
+      <progress max="100" value="45"></progress><br></div>
+      <div class="cajaAbajo">
+        <b>Información Adicional</b><br>
+        <p>
+          </p><p><b>Nombre</b>: Ejemplo</p>
+          <p><b>Apellidos</b>: Ejemplo</p>
+          <p><b>DNI</b>: Ejemplo</p>
+          <p><b>Estudios</b>: DAW</p>
+          <p><b>Correo</b>: yomismo@yomismo.cat</p>
+          <p><b>Ciudad</b>: Valencia</p>
+          <p></p>
+          </div>
     </div>
-    <br>
-
-    <a href="./editarPerfil.php"><input class="boton1" type="submit" name="editarPerfil" value="Editar Perfil"></a><br>
-    <a href="./main.php"><input class="boton2" type="submit" name="volver" value="Volver"></a>
-    <br>
-    <img id="nivel" src="./<?php echo $setInfo[11]?>" alt="">
-    <div class="nivel"><b>Nivel</b><br>
-    <?php echo $setInfo[7]?></div>
-    <div class="puntosAc"><b>Puntos Actuales</b>
-    <br><?php echo $setInfo[5]?></div>
-    <div class="puntosNe"><b>Puntos necesarios siguiente nivel</b><br>
-    <?php $diferencia=$setInfo[10]-$setInfo[5]; echo $diferencia; ?></div>
-    <div class="ProcesoNivel"><b>Proceso de nivel:</b><br><?php  $total = $setInfo[5]*100/$setInfo[10]; echo $total;
-    ?>%</div>
-    <progress max="100" value="<?php echo $total ?>"></progress><br>
-  <br>
-  <div class="adInfo"><b>Información Adicional</b><br></div>
-  <div id="centro">
-  <p>
-    </p><p><b>Nombre</b>:<?php echo $setInfo[0]  ?></p>
-    <p><b>Apellidos</b>: <?php echo $setInfo[1] ?></p>
-    <p><b>Estudios</b>: <?php echo $setInfo[2] ?></p>
-    <p><b>Correo</b>:<?php echo $setInfo[3] ?></p>
-  </p>
-    </div>
-
-
-</body></html>
+</body>
+</html>
